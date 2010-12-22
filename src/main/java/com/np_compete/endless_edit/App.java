808a4dcf388f07ccbc7ecba489f0c2b095de1cfc
Dan Hagon <axiomsofchoice@gmail.com>
2010-12-22 22:37:26 +0000

    Created an new project based on a simple Maven artifact.

---
+++ src/main/java/com/np_compete/endless_edit/App.java
@@ -0,0 +1,13 @@
+package com.np_compete.endless_edit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
