07a7d9917aac756840554740209b57555c2b6ac4
Dan Hagon <axiomsofchoice@gmail.com>
2010-12-22 23:22:02 +0000

    Described in broad outline the crucial role played by collaborators in the system.

---
+++ src/test/java/com/np_compete/endless_edit/Collaborator.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.np_compete.endless_edit;
+
+/**
+ * <p>Collaborators are the users of the system. They may either
+ * be real or bots, in the latter case entirely interacting with
+ * the system via an API.
+ * 
+ * A collaborator must declare one or more goals for their
+ * participation in the system. By satisfying goals to varying
+ * degrees collaborators gain points with the meta-goal of
+ * obtaining higher and higher scores.</p>
+ * 
+ * @author axiomsofchoice
+ *
+ */
+public class Collaborator {
+
+}
