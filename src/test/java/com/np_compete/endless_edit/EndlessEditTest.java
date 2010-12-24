c398a9878d4f592dd02e3ec9413cefa6a19b9db7
Dan Hagon <axiomsofchoice@gmail.com>
2010-12-24 21:45:26 +0000

    Corrected the unit test set up.

---
+++ src/test/java/com/np_compete/endless_edit/EndlessEditTest.java
@@ -0,0 +1,29 @@
+package com.np_compete.endless_edit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Basic unit test for Endless Edit.
+ * 
+ * @author axiomsofchoice
+ */
+public class EndlessEditTest {
+	
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public EndlessEditTest( String testName ) {
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp() {
+        assertTrue( true );
+    }
+}
